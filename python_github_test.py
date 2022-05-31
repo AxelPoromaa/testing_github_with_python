b38af3a792f3614461ce963f00f416d4418099d7
Axel Poromaa <axel.poromaa@gmail.com>
2022-05-31 17:25:25 +0200

    added some prints and a for loop

--- python_github_test.py
+++ python_github_test.py
@@ -1,3 +1,12 @@
 print("Hello github")
 
 print("testing merge to master from branch")
+
+
+Print("On a new branch (testing_new_branch)")
+print("also added a for loop")
+
+x = [1, 2, 3]
+
+for y in x:
+    print(x)
